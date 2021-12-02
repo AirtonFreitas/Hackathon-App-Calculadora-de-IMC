b7a576ba99ee6c07b246ad7226235d99d1f9e177
AirtonFreitas <airtonsiqueiradefreitas@gmail.com>
2021-12-02 20:47:04 -0300

    telas de dicas

--- app/src/main/java/com/iasoftwares/imcperfeito/IdealWeightActivity.kt
+++ app/src/main/java/com/iasoftwares/imcperfeito/IdealWeightActivity.kt
@@ -1,11 +1,21 @@
 package com.iasoftwares.imcperfeito
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.iasoftwares.imcperfeito.databinding.ActivityIdealWeightBinding
 
 class IdealWeightActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityIdealWeightBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ideal_weight)
+        binding = ActivityIdealWeightBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnVoltar.setOnClickListener {
+            var mIntent = Intent(this, MainActivity::class.java)
+            startActivity(mIntent)
+            finish()
+        }
     }
 }
--- app/src/main/java/com/iasoftwares/imcperfeito/OverweightActivity.kt
+++ app/src/main/java/com/iasoftwares/imcperfeito/OverweightActivity.kt
@@ -1,11 +1,21 @@
 package com.iasoftwares.imcperfeito
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.iasoftwares.imcperfeito.databinding.ActivityOverweightBinding
 
 class OverweightActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityOverweightBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_overweight)
+        binding = ActivityOverweightBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnVoltar.setOnClickListener {
+            var mIntent = Intent(this, MainActivity::class.java)
+            startActivity(mIntent)
+            finish()
+        }
     }
 }
